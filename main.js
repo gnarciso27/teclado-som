67f4df44eca55367a566ba6978ec47099f0195c9
Marcosa Trigueiro <marcosandre77@gmail.com>
2024-05-27 17:13:38 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function tocaSom(seletorAudio){
+elemento = document.querySelector(seletorAudio);
+if(elemento === null){
+    console.log("elemento não encontrado")
+}if(elemento != null && elemento.localName === 'audio'){
+    elemento.play();
+}else{
+    console.log (" seletor inválido")
+}
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `som_${instrumento}`;
+     
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+}
